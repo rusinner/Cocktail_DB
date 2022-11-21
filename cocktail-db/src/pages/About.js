8a66b6ef5455d4f06a84accaaf32c2b5d879dc46
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-21 16:27:29 +0200

    about and error pages done

--- cocktail-db/src/pages/About.js
+++ cocktail-db/src/pages/About.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+        repellendus illo omnis amet, rem odit natus eum, reprehenderit nisi
+        laudantium dolore unde, aperiam id magnam perferendis! Ex voluptatum
+        odit soluta, nostrum quaerat, eius aperiam voluptate doloribus ratione
+        distinctio odio esse!
+      </p>
+    </section>
   );
 };
 
