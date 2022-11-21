51382602094c54aa926ecc68cb94875f5dbe808c
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-21 15:33:16 +0200

    routes structure

--- cocktail-db/src/App.js
+++ cocktail-db/src/App.js
@@ -14,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
