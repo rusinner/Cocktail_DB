c53fefc2a5fe9e23849db5bbd83ac8d5f897a8c9
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-21 17:32:30 +0200

    8:28:50

--- cocktail-db/src/components/CocktailList.js
+++ cocktail-db/src/components/CocktailList.js
@@ -12,6 +12,16 @@ const CocktailList = () => {
   if (cocktails.length < 1) {
     return <h2 className="section-title">no cocktails match your criteria</h2>;
   }
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default CocktailList;
